f405a8bddc4bdf7cf9106bd773dfb7116b345446
jacobeulogio <benitezjacob35@gmail.com>
2025-06-09 23:15:33 +0800

    First Commit

---
+++ configuration.nix
@@ -0,0 +1,170 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Manila";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_PH.UTF-8";
+    LC_IDENTIFICATION = "en_PH.UTF-8";
+    LC_MEASUREMENT = "en_PH.UTF-8";
+    LC_MONETARY = "en_PH.UTF-8";
+    LC_NAME = "en_PH.UTF-8";
+    LC_NUMERIC = "en_PH.UTF-8";
+    LC_PAPER = "en_PH.UTF-8";
+    LC_TELEPHONE = "en_PH.UTF-8";
+    LC_TIME = "en_PH.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  programs.hyprland = {
+  enable = true;
+  xwayland.enable = true;
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eulogio = {
+    isNormalUser = true;
+    description = "Eulogio";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.zsh = {
+  enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim 
+    neovim
+    gcc
+    wget
+    git
+    gh
+    stow
+    atuin
+    yazi 
+    fzf
+    fd
+    curl
+    wezterm
+    tmux
+    zsh-powerlevel10k
+    oh-my-zsh
+    cmake
+    rofi-wayland
+    waybar
+    p7zip
+    nwg-look
+    pavucontrol
+    qutebrowser
+    vivaldi
+    hyprpaper
+    rofi-wayland
+    waybar
+    blueman
+    networkmanagerapplet
+    cliphist
+    wl-clipboard
+    swaynotificationcenter
+    papirus-icon-theme
+  ];
+
+
+  users.defaultUserShell = pkgs.zsh;
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    nerd-fonts.jetbrains-mono
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Services
+   services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
