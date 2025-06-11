17d743e51415852a31587d7894ec38600a1b71a7
jacobeulogio <benitezjacob35@gmail.com>
2025-06-11 10:11:07 +0800

    Nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.eulogio = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, ...  }: {
+    nixosConfigurations.eulogio = lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
