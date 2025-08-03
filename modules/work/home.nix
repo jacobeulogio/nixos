9e46ad4afc622757ee03e70c0f6ef9c489995327
jacobeulogio <benitezjacob35@gmail.com>
2025-08-03 22:54:24 +0800

    updates

--- modules/work/home.nix
+++ modules/work/home.nix
@@ -1,14 +1,20 @@
 
 { pkgs, lib, ... }: {
+
   home-manager.users.eulogio = {
     home.packages = with pkgs; [
       kubectl
       docker
     ];
 
+    home.sessionVariables = {
+      GOOGLE_CLOUD_PROJECT = "dashboard-441809";
+    };
+
     programs.git = {
       userName = lib.mkForce "jacobbenitez";
       userEmail = lib.mkForce "jacob.benitez@triumphhomedepot.com";
     };
   };
+
 }
