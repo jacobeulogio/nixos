09814050ba1f0fc9136f9cf34dafe2deac780e3e
jacobbenitez <jacob.benitez@triumphhomedepot.com>
2025-08-28 15:00:31 +0800

    pkgs

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -4,48 +4,7 @@
   ...
 }:
 {
-  programs.zsh = {
-    enable = true;
-  };
 
-  # programs.nix-ld.enable = true;
-
-  nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    git
-    gh
-    vim
-    atuin
-    yazi
-    stow
-    wget
-    curl
-    tree
-    wl-clipboard
-    fzf
-    fd
-    ripgrep
-    zoxide
-    p7zip
-    unzip
-    ffmpeg
-    jq
-    poppler
-    resvg
-    imagemagick
-    openssl
-    wezterm
-    tmux
-    zsh-powerlevel10k
-    opencode
-    # (pkgs.callPackage ../../packages/opencode.nix { })
-
-  ];
 
   fonts.packages = with pkgs; [
     noto-fonts
